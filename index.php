67a481e83768e0d7e06d9b70c760f0823a2f628d
PAPERPANKS <pnkjgautam05@gmail.com>
2017-08-18 12:27:55 +0530

    directory updated and redesigned

---
+++ index.php
@@ -0,0 +1,253 @@
+<!DOCTYPE html>
+<html lang="en" class="">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" name="viewport">
+	
+	<title>IPU-Home</title>
+
+	<!--     Fonts and icons     -->
+	<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons"/>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css"/>
+	<!-- CSS Files -->
+    <link href="../assets/css/bootstrap.min.css" rel="stylesheet">
+	<link href="../assets/css/material-kit.min.css" rel="stylesheet">
+
+	<style>
+		.cards .container .row .col-md-6{
+			padding-left:0;
+			padding-right:0;
+		}
+	</style>
+</head>
+<body class="index-page">
+    <?php include ('../php/header.html'); ?>
+
+    <div class="page-header header-filter" data-parallax="true" style="background-image: url(../assets/img/usict.jpg); transform: translate3d(0px, 0px, 0px);">
+        <div class="container">
+            <div class="row">
+				<div class="col-md-6">
+					<h1 class="title">Guru Gobind Singh Indraprastha University</h1>
+                    <h4>Your story start with us.</h4>
+                    <br>
+                </div>
+            </div>
+        </div>
+    </div>
+
+	<div class="main main-raised">
+
+		<div class="section text-center section-landing">
+			<div class="row">
+				<div class="col-md-8 col-md-offset-2">
+					<h2 class="title">Let's talk product</h2>
+					<h5 class="description">This is the paragraph where you can write more details about your product. Keep you user engaged by providing meaningful information. Remember that by this time, the user is curious, otherwise he wouldn't scroll to get here. Add a button if you want the user to see more.</h5>
+				</div>
+			</div>
+		</div><!--section-landing-->
+
+		<div id="cards" class="cd-section">
+			<div class="section-gray">
+				<div class="cards" >
+					<div class="container-fluid">
+						<div class="title">
+							<h2>Cards</h2>
+							<h3>Quick Links</h3>
+						</div>
+						<div class="row">
+							<div class="col-md-2 col-sm-6">
+								<div class="card card-plain">
+									<div class="card-content card-1">
+										<div class="icon">
+											<i class="fa fa-university"></i>
+										</div>
+										<h4 class="card-title">Library</h4>															
+									</div>
+								</div>
+							</div>
+							<div class="col-md-2 col-sm-6">
+								<div class="card card-plain">
+									<div class="card-content card-2">
+										<div class="icon">
+											<i class="fa fa-book"></i>
+										</div>
+										<h4 class="card-title">Exams</h4>									
+									</div>
+								</div>
+							</div>
+							<div class="col-md-2 col-sm-6">
+								<div class="card card-plain">
+									<div class="card-content card-3">
+										<div class="icon">
+											<i class="fa fa-paper-plane-o"></i>
+										</div>
+										<h4 class="card-title">Placement Cell</h4>										
+									</div>
+								</div>
+							</div>
+							<div class="col-md-2 col-sm-6">
+								<div class="card card-plain">
+									<div class="card-content card-4">
+										<div class="icon">
+											<i class="fa fa-users"></i>
+										</div>
+										<h4 class="card-title">Student Welfare</h4>										
+									</div>
+								</div>
+							</div>
+							<div class="col-md-2 col-sm-6">
+								<div class="card card-plain">
+									<div class="card-content card-5">
+										<div class="icon">
+											<i class="fa fa-bell-o"></i>
+										</div>
+										<h4 class="card-title">Notices</h4>
+									</div>
+								</div>
+							</div>	
+							<div class="col-md-2 col-sm-6">
+								<div class="card card-plain">
+									<div class="card-content card-6">
+										<div class="icon">
+											<i class="fa fa-calendar"></i>
+										</div>
+										<h4 class="card-title">Events</h4>										
+									</div>
+								</div>
+							</div>					
+						</div>
+					</div>
+				</div>
+			</div>
+		</div><!--cards-->
+
+		<footer class="footer footer-white footer-big">
+			<div class="container">
+				<div class="content">
+					<div class="row">
+						<div class="col-md-5">
+							<a href="home">
+								<h5>Guru Gobind Singh Indraprastha University</h5>
+							</a>
+							<p>GGSIPU is first University established in 1998 by Govt. of NCT of Delhi under the 
+							provisions of Guru Gobind Singh Indraprastha University Act, 1998 read with its 
+							Amendment in 1999 The University is recognized by (UGC), India under section 12B of 
+							UGC Act. </p>
+						</div>
+						<div class="col-md-2">
+							<h5>About</h5>
+							<ul class="links-vertical">
+								<li>
+									<a href="">Admission
+								</a>
+								</li>
+								<li>
+									<a href="">Examination
+								</a>
+								</li>
+								<li>
+									<a href="">University Schools
+								</a>
+								</li>
+								<li>
+									<a href="">Contact Us
+								</a>
+								</li>
+							</ul>
+						</div>
+						<div class="col-md-2">
+							<h5>Explore</h5>
+							<ul class="links-vertical">
+								<li>
+									<a href="#">Jobs &amp; Oppurtunities
+								</a>
+								</li>
+								<li>
+									<a href="#">Feedback
+								</a>
+								</li>
+								<li>
+									<a href="#">RTI
+								</a>
+								</li>
+								<li>
+									<a href="#">Meet The Team
+								</a>
+								</li>
+								<li>
+									<a href="#">Spoken Tutorial
+								</a>
+								</li>
+								<li>
+									<a href="#">NPTEL
+								</a>
+								</li>
+								<li>
+									<a href="#pablo">Disclaimer
+								</a>
+								</li>
+							</ul>
+						</div>
+						<div class="col-md-3">
+							<h5>Address</h5>
+							<p>Guru Gobind Singh Indraprastha University
+							Sector - 16C Dwarka
+							Delhi - 110078, India<br/>
+							Tel: +91-11-25302170
+							Fax: +91-11- 25302111
+						</p>
+						</div>
+					</div>
+				</div>
+				<hr/>
+				<ul class="social-buttons">
+					<li>
+						<a href="#" class="btn btn-just-icon btn-simple btn-twitter">
+							<i class="fa fa-map"></i>
+						</a>
+					</li>
+					<li>
+						<a href="#" class="btn btn-just-icon btn-simple btn-facebook">
+							<i class="fa fa-facebook-square"></i>
+						</a>
+					</li>                       
+					<li>
+						<a href="#" class="btn btn-just-icon btn-simple btn-google">
+							<i class="fa fa-google-plus"></i>
+						</a>
+					</li>
+					<li>
+						<a href="#" class="btn btn-just-icon btn-simple btn-youtube">
+							<i class="fa fa-youtube-play"></i>
+						</a>
+					</li>
+				</ul>
+				<div class="copyright pull-center">
+					Copyright &copy;<!--script>
+						document.write(new Date().getFullYear()) 
+					</script-->
+					2016 GGSIPU All rights reserved..	
+				</div>
+			</div>
+		</footer>
+
+	</div><!--main-->
+
+	<!--   Core JS Files   -->
+	<script src="../assets/js/jquery.min.js" type="text/javascript"></script>
+	<script src="../assets/js/bootstrap.min.js" type="text/javascript"></script>
+	<script src="../assets/js/material.min.js"></script>
+	<!--    Control Center for Material Kit: activating the ripples, parallax effects-->
+	<script src="../assets/js/material-kit.min.js" type="text/javascript"></script>
+
+	<script type="text/javascript">     
+		$(document).ready(function(){		
+			$(".dropdown").hover(function () {
+				$(this).toggleClass("dropdown open");
+			});
+					
+		});
+    </script>
+</body>
+</html>
