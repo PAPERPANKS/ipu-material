2a744208e2e4a16967657f8ef6d08af6c854ab5d
PAPERPANKS <pnkjgautam05@gmail.com>
2017-08-19 14:04:45 +0530

    icons added to navigation bar

--- index.php
+++ index.php
@@ -29,7 +29,7 @@
             <div class="row">
 				<div class="col-md-6">
 					<h1 class="title">Guru Gobind Singh Indraprastha University</h1>
-                    <h4>Your story start with us.</h4>
+                    <h3>Your story start with us.</h3>
                     <br>
                 </div>
             </div>
@@ -62,7 +62,7 @@
 							<h3>Quick Links</h3>
 						</div>
 						<div class="row">
-							<div class="col-md-2">
+							<div class="col-md-2 col-xs-6">
 								<div class="card card-plain">
 									<div class="card-content card-1">
 										<div class="icon">
@@ -72,7 +72,7 @@
 									</div>
 								</div>
 							</div>
-							<div class="col-md-2">
+							<div class="col-md-2 col-xs-6">
 								<div class="card card-plain">
 									<div class="card-content card-2">
 										<div class="icon">
@@ -82,7 +82,7 @@
 									</div>
 								</div>
 							</div>
-							<div class="col-md-2">
+							<div class="col-md-2 col-xs-6">
 								<div class="card card-plain">
 									<div class="card-content card-3">
 										<div class="icon">
@@ -92,7 +92,7 @@
 									</div>
 								</div>
 							</div>
-							<div class="col-md-2">
+							<div class="col-md-2 col-xs-6">
 								<div class="card card-plain">
 									<div class="card-content card-4">
 										<div class="icon">
@@ -102,7 +102,7 @@
 									</div>
 								</div>
 							</div>
-							<div class="col-md-2">
+							<div class="col-md-2 col-xs-6">
 								<div class="card card-plain">
 									<div class="card-content card-5">
 										<div class="icon">
@@ -112,7 +112,7 @@
 									</div>
 								</div>
 							</div>	
-							<div class="col-md-2">
+							<div class="col-md-2 col-xs-6">
 								<div class="card card-plain">
 									<div class="card-content card-6">
 										<div class="icon">
@@ -128,115 +128,9 @@
 			</div>
 		</div><!--cards-->
 
-		<footer class="footer footer-white footer-big">
-			<div class="container">
-				<div class="content">
-					<div class="row">
-						<div class="col-md-5 col-sm-12">
-							<a href="home">
-								<h5>Guru Gobind Singh Indraprastha University</h5>
-							</a>
-							<p>GGSIPU is first University established in 1998 by Govt. of NCT of Delhi under the 
-							provisions of Guru Gobind Singh Indraprastha University Act, 1998 read with its 
-							Amendment in 1999 The University is recognized by (UGC), India under section 12B of 
-							UGC Act. </p>
-						</div>
-						<div class="col-md-2 col-sm-6">
-							<h5>About</h5>
-							<ul class="links-vertical">
-								<li>
-									<a href="">Admission
-								</a>
-								</li>
-								<li>
-									<a href="">Examination
-								</a>
-								</li>
-								<li>
-									<a href="">University Schools
-								</a>
-								</li>
-								<li>
-									<a href="">Contact Us
-								</a>
-								</li>
-							</ul>
-						</div>
-						<div class="col-md-2 col-sm-6">
-							<h5>Explore</h5>
-							<ul class="links-vertical">
-								<li>
-									<a href="#">Jobs &amp; Oppurtunities
-								</a>
-								</li>
-								<li>
-									<a href="#">Feedback
-								</a>
-								</li>
-								<li>
-									<a href="#">RTI
-								</a>
-								</li>
-								<li>
-									<a href="#">Meet The Team
-								</a>
-								</li>
-								<li>
-									<a href="#">Spoken Tutorial
-								</a>
-								</li>
-								<li>
-									<a href="#">NPTEL
-								</a>
-								</li>
-								<li>
-									<a href="#pablo">Disclaimer
-								</a>
-								</li>
-							</ul>
-						</div>
-						<div class="col-md-3 col-sm-12">
-							<h5>Address</h5>
-							<p>Guru Gobind Singh Indraprastha University<br/>Sector-16C Dwarka Delhi - 110078, India<br/>
-							Tel: +91-11-25302170<br/>
-							Fax: +91-11- 25302111
-						</p>
-						</div>
-					</div>
-				</div>
-				<hr/>
-				<ul class="social-buttons">
-					<li>
-						<a href="#" class="btn btn-just-icon btn-simple btn-twitter">
-							<i class="fa fa-map"></i>
-						</a>
-					</li>
-					<li>
-						<a href="#" class="btn btn-just-icon btn-simple btn-facebook">
-							<i class="fa fa-facebook-square"></i>
-						</a>
-					</li>                       
-					<li>
-						<a href="#" class="btn btn-just-icon btn-simple btn-google">
-							<i class="fa fa-google-plus"></i>
-						</a>
-					</li>
-					<li>
-						<a href="#" class="btn btn-just-icon btn-simple btn-youtube">
-							<i class="fa fa-youtube-play"></i>
-						</a>
-					</li>
-				</ul>
-				<div class="copyright pull-center">
-					Copyright &copy;<!--script>
-						document.write(new Date().getFullYear()) 
-					</script-->
-					2016 GGSIPU All rights reserved..	
-				</div>
-			</div>
-		</footer>
+		<?php include ('assets/html/footer.html'); ?>
 
-	</div><!--main-->
+	</div><!--main-raised-->
 
 	<!--   Core JS Files   -->
 	<script src="assets/js/jquery.min.js" type="text/javascript"></script>
